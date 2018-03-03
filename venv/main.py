3d3839759d3d8a7f182cd56a184022de21f2a107
wfong <ryan.kuan@wsu.edu>
2018-03-03 15:09:31 -0800

    Splits a string and gets syllables

--- venv/main.py
+++ venv/main.py
@@ -2,42 +2,9 @@ from tkinter import*
 import random
 import time
 import requests
+import syllables
 
 #json_stuff = 'http://api.datamuse.com/words?sp=example&d=sp&md=fr&max=1' | python -mjson.tool
-
-# Split string base
-SPLIT = ("; |,.!?")
-
-# Takes the syllable in string form and finds the correct audio file based on it
-class Syllable:
-    length = 1
-    pitch = 1
-    audio = "SomeAudioFile"     # Not a string
-    start_time = 0.0
-
-    def __init__(self, audio_name):
-        self.audio = audio_name
-        return
-
-<<<<<<< HEAD
-    def Activate(self):
-        #Run audio based on length and pitch
-        return
-
-=======
-#added comment to test github
->>>>>>> ef8a1d065796ebe26ee00351da665ae6780fa95a
-
-# Takes a StringVar() and converts it into a list of words
-def SplitInput(input_val):
-    user_input = str(input_val.get())
-    words_list = user_input.split(SPLIT)
-    return words_list
-
-def GetWordsList(display_input):
-    words_list = SplitInput(display_input)
-    print(words_list)
-    return
 
 def main():
     root = Tk()
@@ -50,10 +17,12 @@ def main():
     Tops = Frame(root, width = 1600, height = 600, bg = "red", relief = SUNKEN)
     Tops.pack(side = TOP)
 
+    # Gets text and puts it into display_input
     txtDisplay = Entry(Tops, font=('arial', 20, 'bold'), textvariable = display_input, bd = 30, insertwidth = 4, bg = "powder blue", justify = 'left')
     txtDisplay.grid(columnspan = 4)
 
-    enter_button = Button(Tops, padx = 16, pady = 16, bd = 8, fg = "black", font = ('arial', 20, 'bold'), text = "Enter", bg = "powder blue", command = lambda: GetWordsList(display_input)).grid(row = 2, column = 0)
+    # Enter button
+    enter_button = Button(Tops, padx = 16, pady = 16, bd = 8, fg = "black", font = ('arial', 20, 'bold'), text = "Enter", bg = "powder blue", command = lambda: syllables.SplitSyllables(display_input)).grid(row = 2, column = 0)
 
     root.mainloop()
     return
--- venv/syllables.py
+++ venv/syllables.py
@@ -1,8 +1,53 @@
 import urllib.request
 import json
+import re
+
+# Split string base
+SPLIT = ('\s|(?<!\d)[,.!?]')
+
+# Takes the syllable in string form and finds the correct audio file based on it
+class Syllable:
+    length = 1
+    pitch = 1
+    audio = "SomeAudioFile"     # Not a string
+    start_time = 0.0
+
+    def __init__(self, audio_name):
+        self.audio = audio_name
+        return
+
+    def Activate(self):
+        #Run audio based on length and pitch
+        return
+
+
+# Takes a StringVar() and converts it into a list of words
+def SplitInput(input_val):
+    user_input = str(input_val.get())
+    words_list = re.split(SPLIT, user_input)
+
+    return words_list
+
+# Takes a StringVar() and makes it into a list of syllables
+def SplitSyllables(display_input):
+    words_list = SplitInput(display_input)
+    syllable_list = []
+
+    for word in words_list:
+        if word != "":
+            word_syllables = Syllabize(word)
+            if (word_syllables == None):
+                print("%s could not be parsed.")
+            else:
+                for syllable in word_syllables:
+                    syllable_list.append(syllable)
+
+    for s in syllable_list:
+        print(s)
+    return
 
 # Ideal case: inputs a string, returns lexical stress pronunciation list if it can, otherwise returns None
-def Syllablize(input):
+def Syllabize(input):
 
     # Attempt to fetch url for API
     try:
@@ -22,3 +67,5 @@ def Syllablize(input):
     output = output[:-1]
 
     return output
+
+
