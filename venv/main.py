8e8ce2dd97a9bb5cd6216d8f3fd801c6c792ffdf
wfong <ryan.kuan@wsu.edu>
2018-03-03 10:59:14 -0800

    Test pycharm

---
+++ venv/main.py
@@ -0,0 +1,20 @@
+from tkinter import*
+import random
+import time
+
+root = Tk()
+root.geometry("1600x800+0+0")
+root.title("Vocaloid English")
+
+Tops = Frame(root, width = 1600, height = 200, bg = "red", relief = SUNKEN)
+Tops.pack(side = TOP)
+
+f1 = Frame(root, width = 800, height = 700, bg = "powder blue", relief = SUNKEN)
+f1.pack(side = LEFT)
+
+f2 = Frame(root, width = 300, height = 700, bg = "powder blue", relief = SUNKEN)
+f2.pack(side = RIGHT)
+
+
+
+root.mainloop()
